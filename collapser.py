cdfc7936c67614555553e7d89082afa5c9da024b
mithunpaul <mithunpaul08@gmail.com>
2018-10-19 15:08:56 -0700

    subsumption works

--- collapser.py
+++ collapser.py
@@ -51,14 +51,17 @@ def get_new_name(prev, unique_new_ners, curr_ner, dict_tokenner_newner, curr_wor
 
     return prev, dict_tokenner_newner, new_sent, full_name,unique_new_ners,unique_new_tokens,dict_newner_token
 
-def check_exists_in_claim(new_sent_after_collapse, dict_tokenner_newner_evidence ,dict_newner_token,dict_tokenner_newner_claims):
+def check_exists_in_claim(new_ev_sent_after_collapse, dict_tokenner_newner_evidence, dict_newner_token_ev, dict_tokenner_newner_claims):
 
     combined_sent=[]
 
 
-    for word in new_sent_after_collapse:
-        if word in dict_newner_token.keys():
-            token=dict_newner_token[word]
+    for ev_new_ner_value in new_ev_sent_after_collapse:
+        #while parsig through the new evidence sentence you might encounter a new NER tag (eg: PER-E1). find its corresponding string value Eg: "tolkein"
+        if ev_new_ner_value in dict_newner_token_ev.keys():
+
+            #find its corresponding string value Eg: "tolkein"
+            token=dict_newner_token_ev[ev_new_ner_value]
 
             token_split=set(token.split(" "))
             #print(token_split)
@@ -66,28 +69,51 @@ def check_exists_in_claim(new_sent_after_collapse, dict_tokenner_newner_evidence
             found_intersection=False
             for tup in dict_tokenner_newner_claims.keys():
                 name_cl = tup[0]
+                ner_cl=tup[1]
                 name_cl_split = name_cl.split(" ")
                 #print("first value in tuples is")
                 #print(name_cl_split)
 
-                if (token_split.intersection(name_cl_split)):
+                #if (token_split.intersection(name_cl_split)):
+                if (token_split.issubset(name_cl_split)):
                     #print("name exists")
-                    val_claim = dict_tokenner_newner_claims[tup]
-                    combined_sent.append(val_claim)
-                    found_intersection=True
+
+
+                    # also confirm that NER value also matches. This is to avoid john amsterdam PER overlapping with AMSTERDAM LOC
+                    actual_ner_tag=""
+                    for k, v in dict_tokenner_newner_evidence.items():
+
+                        if (ev_new_ner_value == v):
+
+                            # print(new_ner_value)
+                            # print(k, v)
+                            actual_ner_tag=k[1]
+                            #print("the value of actual_ner_tag is:"+str(actual_ner_tag))
+
+                            break
+
+                    #now check if this NER tag in evidence also matches with that in claims
+                    if(actual_ner_tag==ner_cl):
+
+                        val_claim = dict_tokenner_newner_claims[tup]
+                        combined_sent.append(val_claim)
+                        found_intersection=True
+
             if not (found_intersection):
-                combined_sent.append(word)
+                combined_sent.append(ev_new_ner_value)
                 new_ner=""
+                #get the evidence's PER-E1 like value
                 for k,v in dict_tokenner_newner_evidence.items():
                     #print(k,v)
-                    if(word==v):
+                    if(ev_new_ner_value==v):
                         new_ner=k[1]
-                dict_tokenner_newner_claims[token, new_ner] = word
+
+                dict_tokenner_newner_claims[token, new_ner] = ev_new_ner_value
 
 
 
         else:
-            combined_sent.append(word)
+            combined_sent.append(ev_new_ner_value)
 
 
     print(combined_sent)
@@ -140,9 +166,18 @@ def collapse_both(claims_words_list,claims_ner_list,ev_claim):
 
 
 if __name__=="__main__":
-    claims_words_list = ["Nikolaj", "Coster-Waldau", "worked", "with", "the", "Fox", "Broadcasting", "Company", "."]
+
+
     claims_ner_list = ["PERSON", "PERSON", "O", "O", "O", "ORGANIZATION", "ORGANIZATION", "ORGANIZATION", "O"]
-    evidence_words_list =  ["He", "then", "played", "Detective", "John", "Amsterdam", "in", "the", "short-lived", "Fox", "television", "series", "New", "Amsterdam", "-LRB-", "2008", "-RRB-", ",", "as", "well", "as", "appearing", "as", "John", "Pike", "in", "the", "2009", "Fox", "television", "film", "Virtuality", ",", "originally", "intended", "as", "a", "pilot", "."]
+
+    claims_words_list = ["John", "Amsterdam", "worked", "with", "the", "Fox", "Broadcasting", "Company", "."]
+    evidence_words_list = ["He", "then", "played", "Detective", "John", "Amsterdam", "in", "the", "short-lived", "Fox",
+                           "television", "series", "New", "Amsterdam", "-LRB-", "2008", "-RRB-", ",", "as", "well",
+                           "as", "appearing",
+                           "as", "John", "Pike", "in", "the", "2009", "Fox", "television", "film", "Virtuality", ",",
+                           "originally", "intended", "as", "a", "pilot", "."]
+
+
     evidence_ner_list = ["O", "O", "O", "O", "PERSON", "PERSON", "O", "O", "O", "O", "O", "O", "O", "LOCATION", "O", "DATE", "O", "O", "O", "O", "O", "O", "O", "PERSON", "PERSON", "O", "O", "DATE", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
     # claims_words_list = ["Roman", "Atwood", "is", "a", "content", "creator", "."]
@@ -169,14 +204,14 @@ if __name__=="__main__":
 
 
     ev_claim = "e"
-    new_sent_after_collapse, dict_tokenner_newner_evidence ,dict_newner_token= collapse_both(evidence_words_list, evidence_ner_list, ev_claim )
+    new_sent_after_collapse, dict_tokenner_newner_evidence ,dict_newner_token_ev= collapse_both(evidence_words_list, evidence_ner_list, ev_claim )
     #print("dict_newner_token is:" + str(dict_newner_token))
 
 
     #print("new_sent_after_collapse")
     #print(new_sent_after_collapse)
     print(evidence_words_list)
-    check_exists_in_claim(new_sent_after_collapse, dict_tokenner_newner_evidence, dict_newner_token,dict_tokenner_newner_claims)
+    check_exists_in_claim(new_sent_after_collapse, dict_tokenner_newner_evidence, dict_newner_token_ev,dict_tokenner_newner_claims)
 
 
     print("done")
